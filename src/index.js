8897569df81bdaf8d1c12e06de1d7315a19f935c
Talita Lira <lira.talita@hotmail.com>
2018-05-07 23:55:51 -0300

    changing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 // create a new component. This component should produce some HTML
 
-const App = function() {
-    return <div>Hi!</div>;
+const App = () => {
+    return <div>Let's get ready!</div>;
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
