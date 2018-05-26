9a472b7b378ca59b21e319db474e90d8fdf15675
Talita Lira <lira.talita@hotmail.com>
2018-05-26 13:11:22 -0300

    added main video and clickable list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,8 +5,21 @@ import React, { Component } from 'react';
 // }
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div className="search-bar col-md-8">
+                <input 
+                    value={this.state.term}
+                    onChange={event => console.log(event.target.value)} 
+                />
+            </div>
+        );
     }
 
 
